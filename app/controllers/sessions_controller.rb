363c7e8c5a30dd158c0285582634a180ad97139e
Adam Bridge <bridge.adam@gmail.com>
2024-06-18 13:18:13 -0400

    Basic insecure login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find params[:id]
+    if @user
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to login_path
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get "login" =>"sessions#new"
   resources :calls
   resources :coaches
   resources :students
   resources :users
+  resources :sessions
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
