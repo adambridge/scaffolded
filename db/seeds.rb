f06747f8a7728ae4c3973b9d5570d530122dcc8a
Adam Bridge <bridge.adam@gmail.com>
2024-06-19 13:58:49 -0400

    Seed a user with no calls

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,7 @@ alice = Coach.create!(name: "Alice", phone: "001-456-7890")
 bob = Student.create!(name: "Bob", phone: "002-456-7890")
 chris = Coach.create!(name: "Chris", phone: "003-456-7890")
 diane = Student.create!(name: "Diane", phone: "004-456-7890")
+emily = Coach.create!(name: "Emily", phone: "004-456-7890")
 
 Call.create!(coach: alice, student: bob, start: Time.zone.now + 1.day)
 Call.create!(coach: chris, student: diane, start: Time.zone.now + 2.days)
