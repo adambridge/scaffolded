2b9093d347d3cca688ed9a7fbfa54c7553d2cafc
Adam Bridge <bridge.adam@gmail.com>
2024-06-20 11:56:20 -0400

    Add coach? helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find session[:user_id] if session[:user_id]
   end
   helper_method :current_user
+
+  def coach?
+    current_user.type == "Coach"
+  end
+  helper_method :coach?
 end
