d90374df5d4418bb18f38aa1551fe5ab414af7d8
Adam Bridge <bridge.adam@gmail.com>
2024-06-20 11:32:05 -0400

    Validate call overlap

--- app/models/call.rb
+++ app/models/call.rb
@@ -1,4 +1,22 @@
 class Call < ApplicationRecord
   belongs_to :coach
   belongs_to :student, optional: true
+
+  validate :overlap
+  validates :start, presence: true
+
+  # Currently assumed that meetings are always 2 hours long
+  DURATION = 2.hours
+
+  private
+
+  def overlap
+    return unless start
+
+    finish = start + DURATION
+    two_hours_before_start = start - DURATION
+    if !Call.where(coach: coach, start: two_hours_before_start..finish).empty?
+      errors.add(:start, " overlaps another meeting")
+    end
+  end
 end
