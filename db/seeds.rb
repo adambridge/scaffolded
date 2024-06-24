634d71de59defb7882b46d1af90ba282559a8067
Adam Bridge <bridge.adam@gmail.com>
2024-06-24 10:43:31 -0400

    Put past calls, rating and notes in seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,6 +14,15 @@ chris = Coach.create!(name: "Chris", phone: "1-800-CHRIS")
 diane = Student.create!(name: "Diane", phone: "1-800-DIANE")
 emily = Coach.create!(name: "Emily", phone: "1-800-EMILY")
 
+Call.create!(coach: alice, student: bob, start: Time.zone.now - 2.days, satisfaction: 4,
+  notes: <<~NOTES
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+  NOTES
+)
+Call.create!(coach: alice, student: bob, start: Time.zone.now - 1.day)
 Call.create!(coach: alice, student: bob, start: Time.zone.now + 1.day)
 Call.create!(coach: alice, start: Time.zone.now + 2.day)
 Call.create!(coach: chris, student: diane, start: Time.zone.now + 2.days)
