2bdb6ee80738c682738a37adb6d5ef8d101624e7
Adam Bridge <bridge.adam@gmail.com>
2024-06-24 10:13:48 -0400

    Treat Users as Students by default

--- app/models/calls_provider_factory.rb
+++ app/models/calls_provider_factory.rb
@@ -1,12 +1,13 @@
 class CallsProviderFactory
   TYPES = {
     "Coach" => CoachCallsProvider,
-    "Student" => StudentCallsProvider
+    "Student" => StudentCallsProvider,
+    nil => StudentCallsProvider
   }
 
   def self.for(user)
     return unless user
-    
+
     TYPES[user.type].new(user)
   end
 end
